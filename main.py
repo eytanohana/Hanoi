5a44d3ab68e5da2a446d04fadd5f509d7c39dfde
eytanohana <eohana98@gmail.com>
2020-12-29 20:48:32 +0200

    implement hanoi

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 
 def hanoi(discs=3):
-    def _hanoi(discs, from_, to, via):
-        pass
+    def _hanoi(disc, from_, to, via):
+        if disc <= 1:
+            print(f'Move disc {disc} {from_} to {to}.')
+            yield disc, from_, to
+        else:
+            yield from _hanoi(disc - 1, from_,via, to)
+            print(f'Move disc {disc} {from_} to {to}.')
+            yield disc, from_, to
+            yield from _hanoi(disc - 1, via, to, from_)
+
     return _hanoi(discs, 1, 3, 2)
 
 
 if __name__ == '__main__':
     for move in hanoi(3):
-        print(move)
+        pass
