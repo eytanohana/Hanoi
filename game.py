43981a83308371dcb15c98161c9c06c2854a4c40
eytanohana <eohana98@gmail.com>
2020-12-29 23:29:09 +0200

    start graphics

---
+++ game.py
@@ -0,0 +1,21 @@
+import pygame
+
+WIDTH = 800
+HEIGHT = 500
+
+LEFT = int(0.1 * WIDTH)
+TOP = int(0.9 * HEIGHT)
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill('#ffffff')
+        pygame.draw.rect(screen, (0, 0, 0),
+                         pygame.Rect(LEFT, TOP, WIDTH - 2 * LEFT, 20))
+        pygame.display.flip()
