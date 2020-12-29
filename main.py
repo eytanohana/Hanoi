ace7b65352a26e3a91ac80c2993b5bc48142186c
eytanohana <eohana98@gmail.com>
2020-12-29 20:42:53 +0200

    start hanoi

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def hanoi(discs=3):
+    def _hanoi(discs, from_, to, via):
+        pass
+    return _hanoi(discs, 1, 3, 2)
+
+
+if __name__ == '__main__':
+    for move in hanoi(3):
+        print(move)
