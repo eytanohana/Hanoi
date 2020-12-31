fc2475b1b82208be8d031a7555fbf48719ade46f
eytanohana <eohana98@gmail.com>
2020-12-31 18:43:12 +0200

    add more colors + fix pegs

--- game.py
+++ game.py
@@ -13,6 +13,12 @@ BOARD_DIMS = BOARD_POS_LEFT, BOARD_POS_TOP, BOARD_WIDTH, BOARD_HEIGHT
 class Color:
     BLACK = (0,) * 3
     WHITE = (255,) * 3
+    RED = (168, 50, 50)
+    ORANGE = (207, 138, 21)
+    YELLOW = (201, 193, 26)
+    GREEN = (70, 189, 19)
+    BLUE = (19, 150, 194)
+    PURPLE = (153, 13, 191)
 
 
 if __name__ == '__main__':
@@ -30,8 +36,10 @@ if __name__ == '__main__':
         pygame.draw.rect(screen, Color.BLACK, board)
 
         # pegs
-        pegs = [pygame.Rect(i * WIDTH // 4, HEIGHT // 2, 5, BOARD_POS_TOP - 250)
+        pegs = [pygame.Rect(i * WIDTH // 4, HEIGHT // 2, 5, board.top - HEIGHT//2)
                 for i in range(1, 4)]
         for peg in pegs:
+            print(f'{peg.midtop}')
             pygame.draw.rect(screen, Color.BLACK, peg)
         pygame.display.flip()
+
