af0fd270926948af725531b9dcc41c8cce39c203
eytanohana <eohana98@gmail.com>
2021-01-01 14:27:14 +0200

    fix discs

--- game.py
+++ game.py
@@ -38,16 +38,20 @@ def display_pegs():
 
 
 def start_round(n_discs=3):
-    disc = pygame.Rect(pegs[0].centerx, board.top - 20, 120, 10)
+    disc = pygame.Rect(0, 0, 0, 0)
+    disc.width = 120
+    disc.height = 10
     disc.centerx = pegs[0].centerx
     disc.bottom = board.top
     discs.append(disc)
     pygame.draw.rect(screen, Color.RED, disc)
-    for i in range(n_discs-1, 0, -1):
-        print(i, discs[n_discs-i-1])
-        disc = pygame.Rect(discs[-1].left + 5, discs[-1].top-10, discs[-1].width-5*(i+1), 10)
+    for i in range(n_discs - 1, 0, -1):
+        print(i, discs[n_discs - i - 1])
+        disc = pygame.Rect(0, 0, 0, discs[-1].height)
+        disc.width = discs[-1].width * 0.9
         disc.centerx = discs[-1].centerx
-        pygame.draw.rect(screen, random.choice(Color.COLORS), disc)
+        disc.bottom = discs[-1].top
+        pygame.draw.rect(screen, random.choice(Color.DISC_COLORS), disc)
         discs.append(disc)
 
 
@@ -64,11 +68,11 @@ if __name__ == '__main__':
 
         screen.fill(Color.WHITE)
         pygame.draw.rect(screen, Color.BLACK, board)
-        pegs = [pygame.Rect(i * WIDTH // 4, HEIGHT // 2, 5, board.top - HEIGHT // 2)
+        pegs = [pygame.Rect(i * WIDTH // 4, PEG_HEIGHT, 5, board.top - PEG_HEIGHT)
                 for i in range(1, 4)]
         display_pegs()
         discs = []
-        start_round()
+        start_round(10)
 
         pygame.display.flip()
         time.sleep(3)
